c2b2d10c77fd33d92abe0786043d1aff2d353814
badgkat <109937927+badgkat@users.noreply.github.com>
2026-04-23 07:33:24 -0700

    feat: scaffold main library project (net472, KDK + CC references)

---
+++ src/KerbalCampaignKit/Core/CampaignKit.cs
@@ -0,0 +1,10 @@
+namespace KerbalCampaignKit.Core
+{
+    /// <summary>
+    /// Public static entry point. Consumer mods call these methods to
+    /// interact with KerbalCampaignKit.
+    /// </summary>
+    public static class CampaignKit
+    {
+    }
+}
